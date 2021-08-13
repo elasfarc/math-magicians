ad56f9b29bb30b01384f68a4b3a003bde2857572
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-13 18:36:30 +0200

    Convert the Screen to a functional component

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -3,11 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Screen.css';
 
-export default class Screen extends React.Component {
-  render() {
-    const { value } = this.props;
-    return <div className="screen">{value}</div>;
-  }
-}
-
+const Screen = ({ value }) => <div className="screen">{value}</div>;
+export default Screen;
 Screen.propTypes = { value: PropTypes.string.isRequired };
