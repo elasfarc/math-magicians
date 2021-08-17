49830b7d08906fbc9e93cae8247795480aae6b83
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-17 22:03:25 +0200

    Add Quote component

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Quote = () => {
-  return <div>QUOTE</div>;
+  const quoteContainerStyles = {
+    width: '80%',
+    maxWidth: '900px',
+    fontSize: ' 3rem',
+    position: 'absolute',
+    top: '40%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+  };
+  const quoteStyles = {};
+  return (
+    <div style={quoteContainerStyles}>
+      <p>
+        Mathematics is not about numbers, equations, computations or algorithms:
+        it is about understanding. -William Paul Thurston
+      </p>
+    </div>
+  );
 };
 
 export default Quote;
